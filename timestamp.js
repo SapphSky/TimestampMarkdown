e3af67389d951257529930f5407163e8be9ce338
SapphSky <44979895+SapphSky@users.noreply.github.com>
2021-07-20 23:11:06 -0400

    Page Design
    - Added a stylesheet to the page. Layout should be better, and easier on the eyes.
    - Reveals the copy button once the markdown has been generated for the first time.
    - Displays a confirmation when the clipboard has been written to, or fails to write to.
    - Shows a warning on browsers with compatibility issues with the Date/Time Local input element.
    - Footer with link to the GitHub page.

--- timestamp.js
+++ timestamp.js
@@ -1,9 +1,21 @@
 (function() {
     console.log(
         "Discord Timestamp Formatter - Sapphire Tools\n" +
-        "Version 1.0.0 | Released 7/20/2021 8:00 PM \n"
-        );
+        "Version 1.0.1"
+    );
+
 })();
+
+function CheckCompatibility() {
+    if (navigator.userAgent.indexOf("Firefox") !== -1) {
+        Notify();
+    }
+}
+
+function Notify() {
+    var warning = document.getElementById("browser-warning");
+    warning.style.display = "block";
+}
 
 function Generate() {
     var output = document.getElementById("output");
@@ -12,14 +24,36 @@ function Generate() {
     var utc = new Date(dt).getTime() / 1000;
 
     output.innerHTML = "<t:" + utc + ":" + format + ">";
+    output.style.display = "inline-block";
+    document.getElementById("copy").style.display = "inline";
 }
 
 function Copy(elementId) {
     var item = document.getElementById(elementId).innerHTML;
-    navigator.clipboard.writeText(item).then(function() {
+    navigator.clipboard.writeText(item).then(
+
+    function() {
+        // Success
         console.log("Copied to clipboard.");
+        document.getElementById("copy_notif").innerHTML = "Copied! :)"
+        document.getElementById("copy_notif").style.color = "greenyellow"
+        Show("copy_notif")
+
     },
     function() {
+        // Failure
         console.log("Copy to clipboard failed!");
+        document.getElementById("copy_notif").innerHTML = "Failed to copy! :("
+        document.getElementById("copy_notif").style.color = "red"
+        Show("copy_notif")
     });
+}
+
+function Show(elementId) {
+    var item = document.getElementById(elementId);
+    item.style.display = "inline";
+
+    setTimeout(() => {
+        item.style.display = "none";
+    }, 5.0 * 1000);
 }
