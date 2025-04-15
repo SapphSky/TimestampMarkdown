49b065bfcbf998b9519ae0d8a92be4d1507f4fca
Joel Fargas <joelfargas@revivn.com>
2025-04-15 07:57:47 -0400

    Styling fix, relative time display

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@vueuse/nuxt",
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
